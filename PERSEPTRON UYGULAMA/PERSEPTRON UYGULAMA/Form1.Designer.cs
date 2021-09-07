87b3fe0b80a4a9ffa3a1d6689153942b0a21ef7c
RabiaYasaroglu <60512074+RabiaYasaroglu@users.noreply.github.com>
2021-09-07 17:05:10 +0300

    Perseptron Model
    adım adım çözüm yöntemi

---
+++ PERSEPTRON UYGULAMA/PERSEPTRON UYGULAMA/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace perseptron
+{
+    partial class Perseptron
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbb2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbb1 = new System.Windows.Forms.TextBox();
+            this.btnSonuç = new System.Windows.Forms.Button();
+            this.tbCozum = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tb_OgrenmeKatsayisi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tb_EsikDegeri = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_w2 = new System.Windows.Forms.TextBox();
+            this.tb_w1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_X2_x2 = new System.Windows.Forms.TextBox();
+            this.tb_X2_x1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_X1_x2 = new System.Windows.Forms.TextBox();
+            this.tb_X1_x1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.Thistle;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.tbb2);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.tbb1);
+            this.panel1.Controls.Add(this.btnSonuç);
+            this.panel1.Controls.Add(this.tbCozum);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.tb_OgrenmeKatsayisi);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.tb_EsikDegeri);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.tb_w2);
+            this.panel1.Controls.Add(this.tb_w1);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tb_X2_x2);
+            this.panel1.Controls.Add(this.tb_X2_x1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tb_X1_x2);
+            this.panel1.Controls.Add(this.tb_X1_x1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.panel1.ForeColor = System.Drawing.Color.Indigo;
+            this.panel1.Location = new System.Drawing.Point(3, 9);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(686, 449);
+            this.panel1.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(236, 89);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 20);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "B2";
+            // 
+            // tbb2
+            // 
+            this.tbb2.BackColor = System.Drawing.Color.White;
+            this.tbb2.Location = new System.Drawing.Point(240, 127);
+            this.tbb2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tbb2.Name = "tbb2";
+            this.tbb2.Size = new System.Drawing.Size(18, 26);
+            this.tbb2.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Indigo;
+            this.label8.Location = new System.Drawing.Point(233, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 20);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "B1";
+            // 
+            // tbb1
+            // 
+            this.tbb1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tbb1.Location = new System.Drawing.Point(240, 46);
+            this.tbb1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tbb1.Name = "tbb1";
+            this.tbb1.Size = new System.Drawing.Size(18, 26);
+            this.tbb1.TabIndex = 3;
+            // 
+            // btnSonuç
+            // 
+            this.btnSonuç.BackColor = System.Drawing.Color.Indigo;
+            this.btnSonuç.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSonuç.ForeColor = System.Drawing.Color.Black;
+            this.btnSonuç.Location = new System.Drawing.Point(14, 375);
+            this.btnSonuç.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.btnSonuç.Name = "btnSonuç";
+            this.btnSonuç.Size = new System.Drawing.Size(244, 43);
+            this.btnSonuç.TabIndex = 11;
+            this.btnSonuç.Text = "Perseptron Çözümü";
+            this.btnSonuç.UseVisualStyleBackColor = false;
+            this.btnSonuç.Click += new System.EventHandler(this.btnSonuç_Click_1);
+            // 
+            // tbCozum
+            // 
+            this.tbCozum.BackColor = System.Drawing.Color.Indigo;
+            this.tbCozum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbCozum.ForeColor = System.Drawing.Color.AliceBlue;
+            this.tbCozum.Location = new System.Drawing.Point(273, 11);
+            this.tbCozum.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tbCozum.Multiline = true;
+            this.tbCozum.Name = "tbCozum";
+            this.tbCozum.ReadOnly = true;
+            this.tbCozum.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbCozum.Size = new System.Drawing.Size(390, 407);
+            this.tbCozum.TabIndex = 25;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(108, 318);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(33, 20);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "ʎ =";
+            // 
+            // tb_OgrenmeKatsayisi
+            // 
+            this.tb_OgrenmeKatsayisi.Location = new System.Drawing.Point(160, 315);
+            this.tb_OgrenmeKatsayisi.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tb_OgrenmeKatsayisi.Name = "tb_OgrenmeKatsayisi";
+            this.tb_OgrenmeKatsayisi.Size = new System.Drawing.Size(39, 26);
+            this.tb_OgrenmeKatsayisi.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(10, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 40);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Öğrenme \r\nKatsayısı";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(108, 257);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 20);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "ɸ =";
+            // 
+            // tb_EsikDegeri
+            // 
+            this.tb_EsikDegeri.Location = new System.Drawing.Point(160, 254);
+            this.tb_EsikDegeri.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tb_EsikDegeri.Name = "tb_EsikDegeri";
+            this.tb_EsikDegeri.Size = new System.Drawing.Size(39, 26);
+            this.tb_EsikDegeri.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(10, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Eşik Değeri";
+            // 
+            // tb_w2
+            // 
+            this.tb_w2.Location = new System.Drawing.Point(187, 200);
+            this.tb_w2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tb_w2.Name = "tb_w2";
+            this.tb_w2.Size = new System.Drawing.Size(21, 26);
+            this.tb_w2.TabIndex = 8;
+            // 
+            // tb_w1
+            // 
+            this.tb_w1.Location = new System.Drawing.Point(160, 200);
+            this.tb_w1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tb_w1.Name = "tb_w1";
+            this.tb_w1.Size = new System.Drawing.Size(21, 26);
+            this.tb_w1.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(108, 166);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(111, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "W =( w1,w2 )";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(10, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ağırlıklar:";
+            // 
+            // tb_X2_x2
+            // 
+            this.tb_X2_x2.Location = new System.Drawing.Point(187, 127);
+            this.tb_X2_x2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tb_X2_x2.Name = "tb_X2_x2";
+            this.tb_X2_x2.Size = new System.Drawing.Size(21, 26);
+            this.tb_X2_x2.TabIndex = 5;
+            // 
+            // tb_X2_x1
+            // 
+            this.tb_X2_x1.Location = new System.Drawing.Point(160, 127);
+            this.tb_X2_x1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tb_X2_x1.Name = "tb_X2_x1";
+            this.tb_X2_x1.Size = new System.Drawing.Size(21, 26);
+            this.tb_X2_x1.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(108, 89);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "X2 =( x1,x2 )";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(10, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "2-) Örnek:";
+            // 
+            // tb_X1_x2
+            // 
+            this.tb_X1_x2.Location = new System.Drawing.Point(187, 46);
+            this.tb_X1_x2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tb_X1_x2.Name = "tb_X1_x2";
+            this.tb_X1_x2.Size = new System.Drawing.Size(21, 26);
+            this.tb_X1_x2.TabIndex = 2;
+            // 
+            // tb_X1_x1
+            // 
+            this.tb_X1_x1.Location = new System.Drawing.Point(160, 46);
+            this.tb_X1_x1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.tb_X1_x1.Name = "tb_X1_x1";
+            this.tb_X1_x1.Size = new System.Drawing.Size(21, 26);
+            this.tb_X1_x1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Indigo;
+            this.label2.Location = new System.Drawing.Point(108, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X1 =( x1,x2 )";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1-) Örnek:";
+            // 
+            // Perseptron
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(694, 477);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Mongolian Baiti", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(200)));
+            this.ForeColor = System.Drawing.Color.Green;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Perseptron";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Perseptron (Basit Algılayıcı Modeli) ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbb2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbb1;
+        private System.Windows.Forms.Button btnSonuç;
+        private System.Windows.Forms.TextBox tbCozum;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tb_OgrenmeKatsayisi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tb_EsikDegeri;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_w2;
+        private System.Windows.Forms.TextBox tb_w1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_X2_x2;
+        private System.Windows.Forms.TextBox tb_X2_x1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_X1_x2;
+        private System.Windows.Forms.TextBox tb_X1_x1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PERSEPTRON UYGULAMA/PERSEPTRON UYGULAMA/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace perseptron
+{
+    public partial class Perseptron : Form
+    {
+        double yeniAgirlikW1, yeniAgirlikW2; 
+        public Perseptron()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSonuç_Click_1(object sender, EventArgs e)
+        {
+            tbCozum.Text = "----PERSEPTRON Adım Adım Çözümü----" + "\r\n";
+            HesapParemetreleri();
+        
+
+        }
+
+
+        public void HesapParemetreleri()
+        {
+            bool islem = true;
+            yeniAgirlikW1 = Convert.ToDouble(tb_w1.Text);
+            yeniAgirlikW2 = Convert.ToDouble(tb_w2.Text);
+            int ornek1Sonuc = 0, ornek2Sonuc = 0;
+            int iterasyon = 1; //
+            while (islem)
+            {
+
+                tbCozum.Text += "\r\n" + iterasyon.ToString() + ".İterasyonda " + "Örnek1 ağa gösterilir";
+                ornek1Sonuc = Formul(
+                                1, iterasyon, Convert.ToDouble(tb_X1_x1.Text), Convert.ToDouble(tb_X1_x2.Text),
+                                yeniAgirlikW1, yeniAgirlikW2,
+                                Convert.ToDouble(tb_OgrenmeKatsayisi.Text), Convert.ToDouble(tbb1.Text)
+                                );
+                iterasyon++;
+                tbCozum.Text += "\r\n" + iterasyon.ToString() + ".İterasyonda " + "Örnek2 ağa gösterilir";
+                ornek2Sonuc = Formul(
+                    2, iterasyon, Convert.ToDouble(tb_X2_x1.Text), Convert.ToDouble(tb_X2_x2.Text),
+                   yeniAgirlikW1, yeniAgirlikW2,
+                   Convert.ToDouble(tb_OgrenmeKatsayisi.Text), Convert.ToDouble(tbb2.Text)
+                   );
+                iterasyon++;
+                if (ornek1Sonuc == 1 && ornek2Sonuc == 1)
+                {
+                    islem = false;
+                }
+            }
+            tbCozum.Text += "\r\n" + "Öğrenme Sonunda ağırlıklar:" + "\r\n" + "w1= " + yeniAgirlikW1.ToString() + "\r\n" + "w2= " + yeniAgirlikW2.ToString();
+        }
+      
+
+
+
+        public int Formul(int ornekNo,int iterasyon,double x1, double x2, double w1,double w2, double a,double beklenen) {
+            double net=0;
+            net = x1 * w1 + x2 * w2;
+            tbCozum.Text+= "\r\n"+ "NET=x1 * w1 + x2 * w2= " + net.ToString() ;//net sonucu
+            if (net > Convert.ToDouble(tb_EsikDegeri.Text))//net sonucu büyükse esik değerinden
+            {
+                tbCozum.Text += "\r\n"+ "Net > 0 olduğundan Ç" + ornekNo.ToString() + "= 1 olacaktır.";      
+                if (beklenen == 1)
+                {
+                    tbCozum.Text += "\r\n" + "Ç" + ornekNo.ToString() + "=B" + ornekNo.ToString() + " olduğundan ağırlıklar değişmez ." + "\r\n";
+                    tbCozum.Text += "\r\nw1=" + w1.ToString() + " w2=" + w2.ToString() + "\r\n";
+                    return 1;
+                    
+                }
+                else
+                {
+                    tbCozum.Text += "\r\n" + "Ç" + ornekNo.ToString() + "=!B" + ornekNo.ToString() + " olduğundan ağırlıklar değişir." + "\r\n";
+
+                    yeniAgirlikW1 = w1 - (a * x1);
+                    tbCozum.Text += "\r\n" + "Yeni Ağırlık(W1)= " + w1.ToString() + "-(" + a.ToString() + "*" + x1.ToString() + ")";
+                    
+                    yeniAgirlikW2 = w2 - (a * x2);
+                    tbCozum.Text += "\r\n" + "Yeni Ağırlık(W2)= " + w2.ToString() + "-(" + a.ToString() + "*" + x2.ToString() + ")" + "\r\n";
+                  
+                    tbCozum.Text += "\r\n" + "-Yeni Değerler-" + "\r\n" + "w1= " + yeniAgirlikW1.ToString() + "\r\n"+ "w2= " + yeniAgirlikW2.ToString() + "\r\n";
+                    return -1;
+                }
+            }
+            else
+            {
+                tbCozum.Text += "\r\n"+ "Net <= ɸ olduğundan Ç" + ornekNo.ToString() + "= 0 olacaktır.";
+                if (beklenen == 0)
+                {
+                    tbCozum.Text += "\r\n" + "Ç" + ornekNo.ToString() + "=B" + ornekNo.ToString() + " olduğundan ağırlıklar değişmez." + "\r\n";
+                    tbCozum.Text += "\r\nw1=" + w1.ToString() + " w2=" + w2.ToString() + "\r\n";
+                    return 1;
+                }
+                else
+                {
+                    tbCozum.Text += "\r\n" + "Ç" + ornekNo.ToString() + "=!B" + ornekNo.ToString() + " olduğundan ağırlıklar değişir." + "\r\n";
+
+                    yeniAgirlikW1 = w1 + (a * x1);
+                    tbCozum.Text += "\r\n" + "Yeni Ağırlık(W1)= " + w1.ToString() + "+(" + a.ToString() + "*" + x1.ToString() + ")";
+                  
+                    yeniAgirlikW2 = w2 + (a * x2);
+                    tbCozum.Text += "\r\n" + "Yeni Ağırlık(W2)= " + w2.ToString() + "+(" + a.ToString() + "*" + x2.ToString() + ")" + "\r\n";
+
+                    tbCozum.Text += "\r\n" + "-Yeni Değerler-" + "\r\n" + "w1= " + yeniAgirlikW1.ToString() + "\r\n"+ "w2= " + yeniAgirlikW2.ToString() + "\r\n";
+                    return -1;
+                    
+                }
+            }
+        }
+
+       
+
+    }
+}
